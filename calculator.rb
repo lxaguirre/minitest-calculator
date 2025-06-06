7d4e9823470ca1ab2cf5258795441afb73897c02
Luis X. Aguirre <149963996+lxaguirre@users.noreply.github.com>
2025-06-06 18:39:03 +0000

    test

---
+++ calculator.rb
@@ -0,0 +1,7 @@
+# calculator.rb
+
+class calculator
+  def add(a, b)
+    a + b
+  end
+end
---
+++ tests/test_caluclator.rb
@@ -0,0 +1,12 @@
+#tests/test_calculator.rb
+
+require 'minitest/autorun'
+require './calculator'
+
+class TestCalculator < Minitest::Test
+  def test_addition
+    calculator = Calculator.new
+    assert_equal 4, calculator.add(2, 2), "Addition method failed"
+  end
+end
+
